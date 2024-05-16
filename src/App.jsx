3fedff455063b57dd5554cc9da73afd5bc3ef484
Luke <apple894894@gmail.com>
2024-05-16 13:21:45 +0800

    submodule update to master

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import CouponLg from "./CouponLg";
 
+const dummy = {
+  domain: "datastore88.myshopify.com",
+  imgUrl: "https://i.imgur.com/SUeDv6E.jpg",
+  productName: "Test Product 1",
+  price: "300",
+  originPrice: "400",
+  color: "#005090",
+  expiration: "Oct 12, 2023",
+};
+
 function App() {
   return (
     <>
       <h1>YouCupon Components</h1>
       <div>
-        <CouponLg />
+        <CouponLg {...dummy} />
       </div>
     </>
   );
--- src/CouponContentInline.jsx
+++ src/CouponContentInline.jsx
@@ -1,5 +1,4 @@
 import dayjs from "dayjs";
-import styles from "./Coupon.module.css";
 import { useMemo } from "react";
 import convert from "color-convert";
 
@@ -9,7 +8,6 @@ const IMAGE_WIDTH_SM = 50;
 const CONTENT_WIDTH_SM = 75;
 const DISCOUNT_WIDTH_SM = 65;
 const MAX_PRICES_LENGTH_SM = 10;
-const PADDING_SM = 2;
 
 // MD: 320x85
 const CONTENT_HEIGHT_MD = 85;
@@ -17,7 +15,6 @@ const IMAGE_WIDTH_MD = 85;
 const CONTENT_WIDTH_MD = 125;
 const DISCOUNT_WIDTH_MD = 110;
 const MAX_PRICES_LENGTH_MD = 12;
-const PADDING_MD = 4;
 
 // LG: 400x120
 const CONTENT_HEIGHT_LG = 120;
@@ -25,10 +22,73 @@ const IMAGE_WIDTH_LG = 120;
 const CONTENT_WIDTH_LG = 150;
 const DISCOUNT_WIDTH_LG = 130;
 const MAX_PRICES_LENGTH_LG = 13;
-const PADDING_LG = 12;
 
 // TODO: remove after confirmed
 const USE_GRADIENT = true;
+
+const couponContentContainerStyle = {
+  width: "100%",
+  height: "100%",
+  position: "absolute",
+  top: 0,
+};
+
+const couponShadowStyle = {
+  filter: "drop-shadow(0px 10px 5px rgba(0, 0, 0, 0.1))",
+};
+
+const couponProductInfoContainerStyle = {
+  display: "flex",
+  flexDirection: "column",
+  flexGrow: 1,
+  justifyContent: "center",
+  alignItems: "center",
+  textAlign: "center",
+  backgroundColor: "white",
+  paddingLeft: "8px",
+};
+
+const couponProductInfoContainerGradientStyle = {
+  display: "flex",
+  flexDirection: "column",
+  flexGrow: 1,
+  justifyContent: "center",
+  alignItems: "center",
+  textAlign: "center",
+  paddingLeft: "8px",
+  paddingRight: "8px",
+};
+
+const couponDiscountInfoContainer = {
+  width: "150px",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  textAlign: "center",
+  color: "white",
+  gap: "2px",
+  paddingLeft: "4px",
+  paddingRight: "8px",
+};
+
+const couponContentLgStyle = {
+  backgroundColor: "white",
+  clipPath:
+    "path('M240.34 0H248.399H256.458H400V2C397.791 2 396 3.79086 396 6C396 8.20914 397.791 10 400 10V14C397.791 14 396 15.7909 396 18C396 20.2091 397.791 22 400 22V26C397.791 26 396 27.7909 396 30C396 32.2091 397.791 34 400 34V38C397.791 38 396 39.7909 396 42C396 44.2091 397.791 46 400 46V50C397.791 50 396 51.7909 396 54C396 56.2091 397.791 58 400 58V62C397.791 62 396 63.7909 396 66C396 68.2091 397.791 70 400 70V74C397.791 74 396 75.7909 396 78C396 80.2091 397.791 82 400 82V86C397.791 86 396 87.7909 396 90C396 92.2091 397.791 94 400 94V98C397.791 98 396 99.7909 396 102C396 104.209 397.791 106 400 106V110C397.791 110 396 111.791 396 114C396 116.209 397.791 118 400 118V120H256.458H248.399H240.34H0V118C2.20914 118 4 116.209 4 114C4 111.791 2.20914 110 0 110V106C2.20914 106 4 104.209 4 102C4 99.7909 2.20914 98 0 98V94C2.20914 94 4 92.2091 4 90C4 87.7909 2.20914 86 0 86V82C2.20914 82 4 80.2091 4 78C4 75.7909 2.20914 74 0 74V70C2.20914 70 4 68.2091 4 66C4 63.7909 2.20914 62 0 62V58C2.20914 58 4 56.2091 4 54C4 51.7909 2.20914 50 0 50V46C2.20914 46 4 44.2091 4 42C4 39.7909 2.20914 38 0 38V34C2.20914 34 4 32.2091 4 30C4 27.7909 2.20914 26 0 26V22C2.20914 22 4 20.2091 4 18C4 15.7909 2.20914 14 0 14V10C2.20914 10 4 8.20914 4 6C4 3.79086 2.20914 2 0 2V0H240.34Z')",
+};
+
+const couponProductNameSpanLgStyle = {
+  width: "100%",
+  textAlign: "left",
+  gap: "2px",
+  // -webkit-box-orient: "vertical",
+  // display: -webkit-box;
+  // -webkit-line-clamp: 3;
+  overflow: "hidden",
+  textOverflow: "ellipsis",
+  whiteSpace: "normal",
+};
 
 /**
  * @param {{
@@ -46,7 +106,7 @@ const USE_GRADIENT = true;
  *    size: ("sm"|"md"|"lg")
  * }} props
  */
-export default function CouponContent(props) {
+export default function CouponContentInline(props) {
   let {
     imgUrl,
     productName,
@@ -59,6 +119,10 @@ export default function CouponContent(props) {
     size = "md",
     shadow,
     url,
+    stampPos = 0,
+    stampType = 0,
+    stampSize = 0,
+    stampBorder = 0,
   } = props;
 
   const clickable = useMemo(() => {
@@ -114,27 +178,7 @@ export default function CouponContent(props) {
     return dayjs(expirationTimestamp).format("MMM DD, YYYY");
   }, [expirationTimestamp]);
 
-  const stampDiv = useMemo(() => {
-    return size == "lg" ? (
-      <div
-        style={{
-          position: "absolute",
-          left: "8px",
-          top: "2px",
-          fontSize: "10px",
-          padding: "2px",
-          backgroundColor: "transparent",
-          color: "#878787",
-        }}
-      >
-        <span>MyShop • YouCupon</span>
-      </div>
-    ) : (
-      <></>
-    );
-  }, [size]);
-
-  const { contentHeight, contentWidth, imageWidth, discountWidth, padding } =
+  const { contentHeight, contentWidth, imageWidth, discountWidth } =
     useMemo(() => {
       return size === "lg"
         ? {
@@ -142,7 +186,6 @@ export default function CouponContent(props) {
             contentWidth: CONTENT_WIDTH_LG,
             imageWidth: IMAGE_WIDTH_LG,
             discountWidth: DISCOUNT_WIDTH_LG,
-            padding: PADDING_LG,
           }
         : size === "md"
         ? {
@@ -150,27 +193,55 @@ export default function CouponContent(props) {
             contentWidth: CONTENT_WIDTH_MD,
             imageWidth: IMAGE_WIDTH_MD,
             discountWidth: DISCOUNT_WIDTH_MD,
-            padding: PADDING_MD,
           }
         : {
             contentHeight: CONTENT_HEIGHT_SM,
             contentWidth: CONTENT_WIDTH_SM,
             imageWidth: IMAGE_WIDTH_SM,
             discountWidth: DISCOUNT_WIDTH_SM,
-            padding: PADDING_SM,
           };
     }, [size]);
+
+  const stampDiv = useMemo(() => {
+    return (
+      <div
+        style={{
+          position: "absolute",
+          left: stampPos === 0 ? "10px" : stampType === 0 ? "210px" : "240px",
+          top: "5px",
+          fontSize: "10px",
+          borderRadius: "4px",
+          // border: "1px solid gray",
+          padding: "2px",
+          backgroundColor: stampBorder === 1 ? color : "transparent",
+          color: stampBorder === 1 ? infoTextColor : "black",
+        }}
+      >
+        {stampType === 0 ? (
+          <span>MyShop • YouCupon</span>
+        ) : stampType === 1 ? (
+          <div className="d-flex flex-column gap-0">
+            <span style={{ height: "10px", lineHeight: "10px" }}>MyShop</span>
+            <span style={{ height: "10px", lineHeight: "10px" }}>•</span>
+            <span style={{ height: "10px", lineHeight: "10px" }}>YouCupon</span>
+          </div>
+        ) : (
+          <div className="d-flex flex-column gap-0">
+            <span style={{ height: "14px", lineHeight: "14px" }}>MyShop •</span>
+            <span style={{ height: "14px", lineHeight: "14px" }}>YouCupon</span>
+          </div>
+        )}
+      </div>
+    );
+  }, [stampPos, stampType, stampBorder]);
 
   const fontSize = size === "lg" ? "16px" : size === "md" ? "14px" : "10px";
   const lineHeight = size === "lg" ? "20px" : size === "md" ? "16px" : "12px";
   return (
     <div
-      className={
-        styles["coupon-content-container"] +
-        " " +
-        (shadow ? styles["coupon-shadow"] : "")
-      }
       style={{
+        ...couponContentContainerStyle,
+        ...(shadow ? couponShadowStyle : {}),
         cursor: clickable ? "pointer" : "auto",
       }}
       onClick={() => {
@@ -180,8 +251,8 @@ export default function CouponContent(props) {
       }}
     >
       <div
-        className={styles["coupon-content-" + size]}
         style={{
+          ...couponContentLgStyle, //TODO
           position: "absolute",
           display: "flex",
           flexDirection: "row",
@@ -194,7 +265,6 @@ export default function CouponContent(props) {
         }}
       >
         <div
-          className={styles["coupon-image-container"]}
           style={{
             width: imageWidth,
             minWidth: imageWidth,
@@ -202,8 +272,8 @@ export default function CouponContent(props) {
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
-            paddingLeft: `${0}px`,
-            paddingTop: `${0}px`,
+            paddingLeft: "8px",
+            paddingTop: "8px",
           }}
         >
           <img
@@ -213,24 +283,28 @@ export default function CouponContent(props) {
               // maxWidth: imageWidth,
               // maxHeight: contentHeight,
               // scale to fill
-              width: imageWidth - padding * 2,
-              height: contentHeight - padding * 2,
+              width: imageWidth - 16,
+              height: contentHeight - 16,
               objectFit: "cover",
             }}
           />
         </div>
         <div
-          className={
-            USE_GRADIENT
-              ? styles["coupon-product-info-container-gradient"]
-              : styles["coupon-product-info-container"]
-          }
           style={{
+            ...(USE_GRADIENT
+              ? couponProductInfoContainerGradientStyle
+              : couponProductInfoContainerStyle),
             width: contentWidth,
           }}
         >
           {stampDiv}
-          <span className={styles["coupon-product-name-span-" + size]}>
+
+          <span
+            style={{
+              ...couponProductNameSpanLgStyle, //TODO
+              // marginTop: "6px",
+            }}
+          >
             {productName}
           </span>
           <div
@@ -256,8 +330,8 @@ export default function CouponContent(props) {
           </div>
         </div>
         <div
-          className={`${styles["coupon-discount-info-container"]}`}
           style={{
+            ...couponDiscountInfoContainer,
             width: discountWidth,
             background: color,
             color: infoTextColor,
