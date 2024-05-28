9725e50de06f4f9bd47f89fcdc200fc05806e1a6
Luke <apple894894@gmail.com>
2024-05-28 09:24:21 +0800

    email files

---
+++ run.sh
@@ -0,0 +1,5 @@
+# Install Bootstrsap Email first by running :
+# gem install bootstrap-email
+# in command line. (Ruby is required)
+rm build/*
+bootstrap-email -p 'coupon-template.html' -d 'build/'
